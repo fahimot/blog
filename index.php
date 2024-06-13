b256a9e63ead338d3c3cae99327cf34e5a01c3c6
fahimot <bakarefahimotblessing@gmail.com>
2024-06-13 14:45:21 +0100

    i added index.php file

---
+++ index.php
@@ -0,0 +1,511 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>blog</title>
+    <link rel="stylesheet" href="css/style.css">
+
+    <div class="boxh">
+    <img src="images/Screenshot-2023-08-17-at-13-30-56-Pasta-More-Reasons-To-Love-A3.cdr-Pasta-More-Reasons-To-Love-728-x-90.pdf-1024x126.png" alt="">
+    </div>
+
+    <div class="fam">
+        <img src="images/Re-branded-business-watch (1).png" alt="">
+        <!-- <h1>BUSINESS <span> WATCH </span></h1> -->
+    </div>
+    
+
+</head>
+<body>
+
+<nav>
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Investors & Analysis</a></li>
+        <li><a href="">Markets</a></li>
+        <li><a href="">Economy</a></li>
+        <li><a href="">Regulators</a></li>
+        <li><a href="">Appointments</a></li>
+        <li><a href="">Business</a></li>
+        <li><a href="">Rankings & Data</a></li>
+        <li><a href="">Brands & Awards</a></li>
+    </ul>
+
+</nav>
+
+<div class="hod">
+    <a href=""><img src="images/image-4-1024x120.png" alt=""></a>
+</div>
+    
+<section id="headline">
+    <div class="com">
+        <div class="headoverlay"></div>
+        <div class="headcontent">
+        <div class="btndiv">
+            <button><a href="" class="button">APPOINTMENTS</a></button>
+        </div>
+        <h4><a href="about.php">Viola Graham-Douglas Named Communications Director at Lafarge Africa</a></h4>
+
+        <h6>In a strategic move aimed at bolstering its communications and sustainability endeavors, Lafarge 
+            Africa, a prominent building materials company operating across Sub-Saharan Africa,
+            has appointed Viola Graham-Douglas…</h6>
+
+            <p>March 26, 2024</p>
+            </div>
+
+            
+    </div>
+</section>
+
+<section class="body">
+    <div class="container">
+        <div class="box1">
+            <div class="box1overlay"></div>
+            <div class="box1content">
+            <button><a href="">APPOINTMENTS</a></button>
+            <h4><a href="">Viola Graham-Douglas Named Communications <br> &nbsp; &nbsp; Director at Lafarge Africa</a></h4>
+            <p>March 26,2024</p>
+            </div>
+            
+        </div>
+
+        <div class="box2">
+            
+            <div class="box2overlay"></div>
+            <div class="box2content">
+            <button><a href="">ECONOMY</a></button>
+            <h4><a href="">Viola Graham-Douglas Named Communications <br> &nbsp; &nbsp; Director at Lafarge Africa</a></h4>
+            <p>March 26,2024</p>
+            </div>
+        </div>
+
+        <div class="box3">
+         <div class="box3overlay"></div>
+            <div class="box3content">
+            <button><a href="">REGULATORS</a></button>
+            <h4><a href="">Viola Graham-Douglas Named Communications <br> &nbsp; &nbsp; Director at Lafarge Africa</a></h4>
+            <p>March 26,2024</p>
+            </div>
+            
+        </div>
+    </div>
+</section>
+
+<section class="stories">
+    <h3>- TOP STORIES -</h3>
+
+    <div class="box1">
+        <div class="story1">
+          <h4><a href="">APPOINTMENTS</a></h4>
+          <h2><a href="">Viola Graham-Douglas Named Communications Director at Lafarge Africa</a></h2>
+          <p>March 26, 2024</p>
+
+          <p>In a strategic move aimed at bolstering its communications and sustainability endeavors, 
+            Lafarge Africa, a prominent building materials company operating across Sub-Saharan Africa,
+            has appointed Viola Graham-Douglas to the…
+          </p>
+        </div>
+
+        <div class="story11">
+            <img src="images/LAFARGE-2.png" alt="">
+        </div>
+
+    </div>
+
+    <div class="box2">
+        <div class="story2">
+          <h4><a href="">APPOINTMENTS</a></h4>
+          <h2><a href="">Viola Graham-Douglas Named Communications Director at Lafarge Africa</a></h2>
+          <p>March 26, 2024</p>
+
+          <p>In a strategic move aimed at bolstering its communications and sustainability endeavors, 
+            Lafarge Africa, a prominent building materials company operating across Sub-Saharan Africa,
+            has appointed Viola Graham-Douglas to the…
+          </p>
+        </div>
+
+        <div class="story22">
+            <img src="images/LAFARGE-2.png" alt="">
+        </div>
+
+    </div>
+
+    <div class="box3">
+        <div class="story3">
+          <h4><a href="">ECONOMY</a></h4>
+          <h2><a href="">Q4 2023 ; Nigeria’s Total Public Debt: A Persistent Ascendancy</a></h2>
+          <p>March 26, 2024</p>
+
+          <p>Quarter Total Public Debt (in trillion Naira) Total Public Debt (in billion USD)
+             Domestic Debt (in trillion Naira) External Debt (in trillion Naira) Q4 2022 46.25 103.11 27.55 18.70…
+          </p>
+        </div>
+
+        <div class="story33">
+            <img src="images/Debt-Capital-Market.png" alt="">
+        </div>
+
+    </div>
+
+    <div class="box4">
+        <div class="story4">
+          <h4><a href="">ECONOMY</a></h4>
+          <h2><a href="">Q4 2023 ; Nigeria’s Total Public Debt: A Persistent Ascendancy</a></h2>
+          <p>March 26, 2024</p>
+
+          <p>Quarter Total Public Debt (in trillion Naira) Total Public Debt (in billion USD)
+             Domestic Debt (in trillion Naira) External Debt (in trillion Naira) Q4 2022 46.25 103.11 27.55 18.70…
+          </p>
+        </div>
+
+        <div class="story44">
+            <img src="images/Debt-Capital-Market.png" alt="">
+        </div>
+
+    </div>
+
+    <div class="box5">
+        <div class="story5">
+            <h4><a href="">REGULATORS</a></h4>
+            <h2><a href="">CBN MPC Raises Interest Rate by 200 basis points to 24.75%</a></h2>
+            <p>March 31, 2024</p>
+
+            <p>In response to ongoing economic challenges, the Central Bank of Nigeria (CBN)
+             took decisive action at its second meeting of 2024. Led by Governor Mr. Olayemi Cardoso, the Monetary…
+            </p>
+        </div>
+        <div class="story55">
+            <img src="images/Olayemi-Cardoso-CBN-Governorc.png" alt="">
+        </div>
+    </div>
+
+    <div class="box6">
+        <div class="story6">
+            <h4><a href="">REGULATORS</a></h4>
+            <h2><a href="">CBN MPC Raises Interest Rate by 200 basis points to 24.75%</a></h2>
+            <p>March 31, 2024</p>
+
+            <p>In response to ongoing economic challenges, the Central Bank of Nigeria (CBN)
+             took decisive action at its second meeting of 2024. Led by Governor Mr. Olayemi Cardoso, the Monetary…
+            </p>
+        </div>
+        <div class="story66">
+            <img src="images/Olayemi-Cardoso-CBN-Governorc.png" alt="">
+        </div>
+    </div>
+
+
+    <h5><a href="">Next Page</a></h5>
+</section>
+
+<section class="eco">
+    <h3>- ECONOMIC NEWS -</h3>
+
+    <div class="cont1">
+        <div class="box">
+            <img src="images/Zenith-Bank-Award-Presentation-scaled.jpg" alt="">
+            <h4>Zenith Bank Clinches Best Bank in Nigeria Title for the Fourth Time at Global Finance Awards 2024</h4>
+            <p>April 2, 2024</p>
+        </div>
+        <div class="box">
+            <img src="images/MG_0996-370x280-1.jpg" alt="">
+            <h4>MRS Oil Nigeria PLC Declares Final Dividend of 236 Kobo per Share</h4>
+            <p>April 2, 2024</p>
+        </div>
+        <div class="box">
+            <img src="images/1711485237186.jpg" alt="">
+            <h4>Idara Udoh Promoted to Marketing Manager for West Africa at 10by10 Styles</h4>
+            <p>April 2, 2024</p>
+        </div>
+
+    </div>
+
+    <div class="cont2">
+        <div class="box">
+            <img src="images/Phoebe-1024x588-1.jpeg" alt="">
+            <h4>Phoebe Dami-Asolo Returns To Nigeria, Assumes New Role As Senior Operation Director, Coca-Cola Nigeria</h4>
+            <p>April 2, 2024</p>
+        </div>
+
+        <div class="box">
+            <img src="images/Dangote-sugar.jpg" alt="">
+            <h4>FY 2023: Dangote Sugar’s Revenue Rises, But Profits Sour</h4>
+            <p>April 1, 2024</p>
+        </div>
+
+        <div class="box">
+            <img src="images/images (1).png" alt="">
+            <h4>Top 10 Nigerian Banks 2023</h4>
+            <p>April 1, 2024</p>
+        </div>
+    </div>
+
+    
+</section>
+
+<section class="market">
+    <h3>- MARKET UPDATES -</h3>
+
+    <div class="cont1">
+        <div class="box">
+            <img src="images/Zenith-Bank-Award-Presentation-scaled.jpg" alt="">
+            <h4>Zenith Bank Clinches Best Bank in Nigeria Title for the Fourth Time at Global Finance Awards 2024</h4>
+            <p>April 2, 2024</p>
+        </div>
+        <div class="box">
+            <img src="images/MG_0996-370x280-1.jpg" alt="">
+            <h4>MRS Oil Nigeria PLC Declares Final Dividend of 236 Kobo per Share</h4>
+            <p>April 2, 2024</p>
+            
+        </div>
+        <div class="box">
+            <img src="images/1711485237186.jpg" alt="">
+            <h4>Idara Udoh Promoted to Marketing Manager for West Africa at 10by10 Styles</h4>
+            <p>April 2, 2024</p>
+        </div>
+
+    </div>
+
+    <div class="cont2">
+        <div class="box">
+            <img src="images/Phoebe-1024x588-1.jpeg" alt="">
+            <h4>Phoebe Dami-Asolo Returns To Nigeria, Assumes New Role As Senior Operation Director, Coca-Cola Nigeria</h4>
+            <p>April 2, 2024</p>
+        </div>
+
+        <div class="box">
+            <img src="images/Dangote-sugar.jpg" alt="">
+            <h4>FY 2023: Dangote Sugar’s Revenue Rises, But Profits Sour</h4>
+            <p>April 1, 2024</p>
+        </div>
+
+        <div class="box">
+            <img src="images/images (1).png" alt="">
+            <h4>Top 10 Nigerian Banks 2023</h4>
+            <p>April 1, 2024</p>
+        </div>
+    </div>
+
+</section>
+
+<section class="appoint">
+    <h3> - APPOINTMENTS -</h3>
+
+    <div class="container">
+        <div class="cont1">
+            <div class="cont1overlay"></div>
+            <div class="cont1content">
+            <button><a href="">INVESTORS & ANALYSIS</a></button>
+            <h4><a href="">FY 2023: Transcorp Group Records Impressive Growth</a></h4>
+            <p>April 4, 2024</p>
+            </div>
+        </div>
+
+        <div class="cont2">
+        <div class="cont2overlay"></div>
+        <div class="cont2content">
+            <button><a href="">BUSINESS</a></button>
+            <h4><a href="">Nigerian Breweries Plans to Raise N600 billion Capital <br> &nbsp; &nbsp; through Existing Shareholders</a></h4>
+            <p>April 3, 2024</p>
+            </div>
+        </div>
+
+        <div class="cont3">
+            <div class="cont3overlay"></div>
+            <div class="cont3content">
+            <button><a href="">APPOINTMENTS</a></button>
+            <h4><a href="">International Breweries PLC Appoints Mrs. Temitope <br> &nbsp; &nbsp; Oguntokun as Executive Director</a></h4>
+            <p>April 2, 2024</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container2">
+        <div class="con1">
+            <div class="con1overlay"></div>
+            <div class="con1content">
+            <button><a href="">BRAND & AWARDS</a></button>
+            <h4><a href="">Zenith Bank Clinches Best Bank in Nigeria Title for the <br> &nbsp; &nbsp; Fourth Time at Global Finance Awards 2024</a></h4>
+            <p>April 2, 2024</p>
+            </div>
+        </div>
+
+        <div class="con2">
+            <div class="con2overlay"></div>
+            <div class="con2content">
+            <button><a href="">MARKETS</a></button>
+            <h4><a href="">MRS Oil Nigeria PLC Declares Final Dividend of 236 <br> &nbsp; &nbsp; Kobo per Share</a></h4>
+            <p>April 2, 2024</p>
+            </div>
+        </div>
+
+        <div class="con3">
+            <div class="con3overlay"></div>
+            <div class="con3content">
+            <button><a href="">APPOINTMENTS</a></button>
+            <h4><a href="">Idara Udoh Promoted to Marketing Manager for West <br> &nbsp; &nbsp; Africa at 10by10 Styles</a></h4>
+            <p>April 2, 2024</p>
+            </div>
+        </div>
+    </div>
+
+
+</section>
+
+<section class="invest">
+    <h3>- INVESTORS AND ANALYSIS -</h3>
+
+    <div class="investor">
+        <div class="invest1">
+            <div class="inv1overlay"></div>
+            <div class="inv1content">
+            <button><a href="">INVESTORS AND ANALYSIS</a></button>
+            <h4><a href="">FY 2023: Transcorp Group Records Impressive Growth</a></h4>
+            <p>April 2, 2024</p>
+            </div>
+        </div>
+       
+
+        <div class="invest2">
+            <div class="inv2overlay"></div>
+            <div class="inv2content">
+            <button><a href="">BUSINESS</a></button>
+            <h4><a href="">Nigerian Breweries Plans to Raise N600 billion Capital through Existing <br> Shareholders</a></h4>
+            <p>April 2, 2024</p>
+            </div>
+        </div>
+        
+
+        <div class="invest3">
+            <div class="inv3overlay"></div>
+            <div class="inv3content">
+            <button><a href="">APPOINTMENTS</a></button>
+            <h4><a href="">International Breweries PLC Appoints Mrs. Temitope Oguntokun as Executive Director</a></h4>
+            <p>April 2, 12024</p>
+            </div>
+        </div>
+
+        <div class="invest4">
+            <div class="inv4overlay"></div>
+            <div class="inv4content">
+            <button><a href="">BRANDS AND AWARDS</a></button>
+            <h4><a href="">Zenith Bank Clinches Best Bank in Nigeria Title for the Fourth Time at Global Finance Awards 2024</a></h4>
+            <p>April 2, 2024</p>
+            </div>
+        </div>
+       
+    </div>
+
+    <div class="investor2">
+        <div class="invest11">
+            <div class="inv11overlay"></div>
+            <div class="inv11content">
+            <button><a href="">MARKETS</a></button>
+            <h4><a href="">MRS Oil Nigeria PLC Declares Final Dividend of 236 Kobo per Share</a></h4>
+            <p>April 2, 2024</p>
+            </div>
+        </div>
+       
+
+        <div class="invest22">
+            <div class="inv22overlay"></div>
+            <div class="inv22content">
+            <button><a href="">APPOINTMENTS</a></button>
+            <h4><a href="">Idara Udoh Promoted to Marketing  <br> Manager for West Africa at 10by10 Styles</a></h4>
+            <p>April 2, 2024</p>
+            </div>
+        </div>
+        
+
+        <div class="invest33">
+            <div class="inv33overlay"></div>
+            <div class="inv33content">
+            <button><a href="">APPOINTMENTS</a></button>
+            <h4><a href="">Phoebe Dami-Asolo Returns To Nigeria, Assumes New Role As Senior Operation Director, Coca-Cola Nigeria</a></h4>
+            <p>April 2, 12024</p>
+            </div>
+        </div>
+
+        <div class="invest44">
+            <div class="inv44overlay"></div>
+            <div class="inv44content">
+            <button><a href="">INVESTORS AND ANALYSIS</a></button>
+            <h4><a href="">FY 2023: Dangote Sugar’s Revenue Rises, But Profits Sour</a></h4>
+            <p>April 2, 2024</p>
+            </div>
+        </div>
+       
+    </div>
+</section>
+
+
+
+<div class="bond">
+    <img src="images/728-x-90-It-tasty-Its-awesome.jpg" alt="">
+</div>
+
+
+<footer>
+    <div class="contain">
+        
+        <div class="box">
+            <h2> - RECENT POSTS -</h2>
+           <div class="lightblack">
+           <!-- <ul>  -->
+                <h4><a href="">Zenith Bank Clinches Best Bank in Nigeria Title <br> for the Fourth Time at Global Finance Awards 2024</a></h4>
+                <p>April 2, 2024</p>
+                <h4><a href="">MRS Oil Nigeria PLC Declares Final Dividend <br> of 236 Kobo per Share</a></h4>
+                <p>April 2, 2024</p>
+                <h4><a href="">Idara Udoh Promoted to Marketing Manager <br> for West Africa at 10by10 Styles</a></h4>
+                <p>April 2,</p>
+            <!-- </ul> -->
+            </div>
+        </div>
+        <div class="box">
+            <h2> - BUSINESS WATCH NIGERIA -</h2>
+            <div class="lightblack">
+            <!-- <ul> -->
+                <h4><a href="">About Us</a></h4>
+                <h4><a href="">Policy</a><h4>
+                <h4><a href="">Terms and Conditions</a></h4>
+                <h4><a href="">Contact Us</a></h4>
+            <!-- </ul> -->
+            </div>
+        </div>
+        <div class="box">
+           <h2> - CATEGORIES -</h2>
+           <div class="lightblack">
+            
+            <!-- <ul> -->
+                <div class="core">
+                    <div class="boy">
+                <h4><a href="">APPOINTMENTS</a></h4>
+                <h4><a href="">BRAND & AWARDS</a></h4>
+                <h4><a href="">BUSINESS</a></h4>
+                <h4><a href="">ECONOMY</a></h4>
+                <h4><a href="">INVESTORS & ANALYSIS</a></h4>
+                <h4><a href="">MARKETS</a></h4>
+                <h4><a href="">RANKING AND DATA</a></h4>
+                <h4><a href="">REGULATORS</a></h4> 
+                </div>
+
+                <div class="boy">
+                    <h4>(7)</h4>
+                    <h4>(8)</h4>
+                    <h4>(3)</h4>
+                    <h4>(7)</h4>
+                    <h4>(14)</h4>
+                    <h4>(8)</h4>
+                    <h4>(1)</h4>
+                    <h4>(3)</h4>
+                </div>
+                </div>
+            <!-- </ul> -->
+        </div>
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
